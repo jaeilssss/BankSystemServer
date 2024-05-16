64fc91b245e0ca4ec3359363ff6effbaa76b8284
jaeilssss <jaeilssss@gmail.com>
2024-05-16 12:27:58 +0900

    ADD Project SetUp

---
+++ JaeilBankSystem/src/main/java/com/example/jaeilbanksystem/JaeilBankSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.jaeilbanksystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaeilBankSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaeilBankSystemApplication.class, args);
+    }
+
+}
---
+++ JaeilBankSystem/src/test/java/com/example/jaeilbanksystem/JaeilBankSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jaeilbanksystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaeilBankSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
