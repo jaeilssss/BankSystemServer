15c042d4c286c84fb0a3e8d6f36ad799d9480214
jaeilssss <jaeilssss@gmail.com>
2024-05-19 21:54:49 +0900

    #4-Add Filter Add
    JWTFIlter Add

--- BankServerSystem/src/main/java/com/example/bankserversystem/config/SecurityConfig.java
+++ BankServerSystem/src/main/java/com/example/bankserversystem/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.bankserversystem.config;
 
+import com.example.bankserversystem.jwt.JwtAuthenticationFilter;
+import com.example.bankserversystem.jwt.JwtProviders;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.HttpSecurityDsl;
@@ -7,13 +9,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private final JwtProviders jwtProviders;
+
+    public SecurityConfig(JwtProviders jwtProviders) {
+        this.jwtProviders = jwtProviders;
+    }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
@@ -24,8 +32,7 @@ public class SecurityConfig {
                     request.requestMatchers("/api/signUp").permitAll();
                     request.anyRequest().authenticated();
                 })
+                .addFilterBefore(new JwtAuthenticationFilter(jwtProviders), UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
-
-
 }
